549830c4ca041f8e13ba095d29da5d247df9561f
Rafaella <rafaella.leticiaa@gmail.com>
2022-01-20 18:34:45 -0300

    Exercicio 3 sobre Spring BackEnd adicionado

--- Bloco2/SpringJAVA/blogpessoal/blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
+++ Bloco2/SpringJAVA/blogpessoal/blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -5,11 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;// Registra exatamente o horário da postagem.
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "Postagem")
@@ -31,6 +34,11 @@ public class Postagem {
 	 @UpdateTimestamp
 	private LocalDateTime data;
 	 
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	 private Tema tema;
+	
+	
 	public Long getId() {
 		return id;
 	}
@@ -55,7 +63,12 @@ public class Postagem {
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
-	
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 	
 
 }
